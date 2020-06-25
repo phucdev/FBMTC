351760f97aece85f30bc428cbe75bec386eb8cd7
Duy Le Thanh <lethanhd@informatik.hu-berlin.de>
2020-06-25 19:18:47 +0200

    Add 'evaluate_predictions()' function

--- fbmtc/pipeline.py
+++ fbmtc/pipeline.py
@@ -4,6 +4,7 @@ from typing import Union, Optional
 import pandas as pd
 from fbmtc import predictor, NEGATIVE_IS_CANCER, NEGATIVE_DOID
 from flair.data import Sentence
+from sklearn.metrics import f1_score
 
 
 def predict_doc(doc, binary_classifier, multi_class_classifier):
@@ -41,3 +42,27 @@ def predict_docs(docs: Union[str, Path],
 
     labeled_docs = pred_docs.apply(lambda doc: predict_doc(doc, binary_classifier, multi_class_classifier), axis=1)
     return labeled_docs
+
+def evaluate_predictions(docs: Union[str, Path],
+                        pred_frame: pd.DataFrame,
+                        target_attribute: str,
+                        average: str = 'micro'):
+    '''
+
+    :param docs: Path to data file that contains true labels for target_attribute
+    :param pred_frame: Pandas.DataFrame that contains target_attribute as column
+    :param target_attribute: Target attribute
+    :param average: Method to calculate f1-score. From https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html:
+        'micro': Calculate metrics globally by counting the total true positives, false negatives and false positives.
+        'macro': Calculate metrics for each label, and find their unweighted mean. This does not take label imbalance into account.
+        'weighted': Calculate metrics for each label, and find their average weighted by support (the number of true instances for each label).
+                    This alters 'macro' to account for label imbalance; it can result in an F-score that is not between precision and recall.
+    :return: F1-score
+    '''
+
+    doc_true = pd.read_csv(docs, sep='\t', header=0)
+    target_true = doc_true[target_attribute].tolist()
+
+    target_pred = pred_frame[target_attribute].tolist()
+
+    return f1_score(target_true, target_pred, average=average)
