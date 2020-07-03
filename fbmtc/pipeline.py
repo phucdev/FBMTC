758e007e612c718e32a027fbe324bb49c84fef54
Phuc Tran Truong <ptruong@outlook.de>
2020-07-03 22:41:25 +0200

    Add progress bar to prediction applier

--- fbmtc/pipeline.py
+++ fbmtc/pipeline.py
@@ -1,17 +1,21 @@
 from pathlib import Path
-from typing import Union, Optional
+from typing import Union
 
 import pandas as pd
 from fbmtc import predictor, NEGATIVE_IS_CANCER, NEGATIVE_DOID
 from flair.data import Sentence
 from sklearn.metrics import f1_score
 
+from tqdm import tqdm
+
 import time
 import sys
 import json
 
-def predict_doc(doc, binary_classifier, multi_class_classifier):
+tqdm.pandas()
 
+
+def predict_doc(doc, binary_classifier, multi_class_classifier):
     binary_result = predictor.predict_instance(binary_classifier, Sentence(doc['text']))['prediction']
 
     if multi_class_classifier is not None:
@@ -23,7 +27,7 @@ def predict_doc(doc, binary_classifier, multi_class_classifier):
             doc['is_cancer'] = NEGATIVE_IS_CANCER
             doc['doid'] = NEGATIVE_DOID
     else:
-        doc['is_cancer']= binary_result
+        doc['is_cancer'] = binary_result
     return doc
 
 
@@ -50,25 +54,31 @@ def predict_docs(docs: Union[str, Path],
         multi_class_classifier = predictor.load_predictor(model_dir=multi_class_classifier_model_dir,
                                                           archive_filename=multi_class_classifier_model_archive)
 
-    labeled_docs = pred_docs.apply(lambda doc: predict_doc(doc, binary_classifier, multi_class_classifier), axis=1)
+    labeled_docs = pred_docs.progress_apply(lambda doc: predict_doc(doc, binary_classifier, multi_class_classifier),
+                                            axis=1)
     return labeled_docs
 
+
 def evaluate_predictions(docs: Union[str, Path],
-                        pred_frame: pd.DataFrame,
-                        target_attribute: str,
-                        average: str = 'micro'):
-    '''
+                         pred_frame: pd.DataFrame,
+                         target_attribute: str,
+                         average: str = 'micro'):
+    """
 
     :param docs: Path to data file that contains true labels for target_attribute
     :param pred_frame: Pandas.DataFrame that contains target_attribute as column
     :param target_attribute: Target attribute
-    :param average: Method to calculate f1-score. From https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html:
-        'micro': Calculate metrics globally by counting the total true positives, false negatives and false positives.
-        'macro': Calculate metrics for each label, and find their unweighted mean. This does not take label imbalance into account.
-        'weighted': Calculate metrics for each label, and find their average weighted by support (the number of true instances for each label).
-                    This alters 'macro' to account for label imbalance; it can result in an F-score that is not between precision and recall.
+    :param average: Method to calculate f1-score.
+        'micro':    Calculate metrics globally by counting the total true positives, false negatives and
+                    false positives.
+        'macro':    Calculate metrics for each label, and find their unweighted mean. This does not take label imbalance
+                    into account.
+        'weighted': Calculate metrics for each label, and find their average weighted by support
+                    (the number of true instances for each label).
+                    This alters 'macro' to account for label imbalance; it can result in an F-score that is not between
+                    precision and recall.
     :return: F1-score
-    '''
+    """
 
     doc_true = pd.read_csv(docs, sep='\t', header=0)
     target_true = doc_true[target_attribute].tolist()
@@ -76,6 +86,7 @@ def evaluate_predictions(docs: Union[str, Path],
     target_pred = pred_frame[target_attribute].tolist()
 
     return f1_score(target_true, target_pred, average=average)
+
 
 if __name__ == "__main__":
     """
@@ -93,9 +104,9 @@ if __name__ == "__main__":
 
     start = time.time()
     df = predict_docs(prediction_file,
-                        binary_classifier_model_dir=binary_model_dir,
-                        multi_class_classifier_model_dir= multi_class_model_dir
-                        )
+                      binary_classifier_model_dir=binary_model_dir,
+                      multi_class_classifier_model_dir=multi_class_model_dir
+                      )
     print('runtime:', '{0:.2f}'.format(time.time() - start), 'seconds')
 
     for i, column in enumerate(export_columns):
